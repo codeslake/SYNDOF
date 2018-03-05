4194a5a5692efc3395eed9c97f94b02318f22a7d
JunyongLee <junyonglee@postech.ac.kr>
2018-03-05 12:20:00 +0900

    completed

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ disp_map_path = './data/disparity_map/'
 save_path = './output/'
 num_layers = 4
 sigma = [9, 6, 3, 0]
+#sigma = [0, 3, 6, 9]
+
+def expand_dims(input_1c):
+	return np.repeat(np.expand_dims(input_1c, 2), 3, axis=2)
 
 def get_image(path, file_name):
     return scipy.misc.imread(path + file_name, mode='RGB')
@@ -19,7 +23,7 @@ def get_disparity_map(path, file_name):
 	return pfm_handler.load_pfm(pfm_file)
 
 def get_decomposed_images(image, disp_map, num_layers):
-	interval_distance = 256 / num_layers
+	interval_distance = 256. / num_layers
 	decomposed_images = []
 	decomposed_disp_maps = []
 	decomposed_disp_maps_binary = []
@@ -46,17 +50,12 @@ def get_decomposed_images(image, disp_map, num_layers):
 	return decomposed_images, decomposed_disp_maps_binary
 
 def masked_gaussian_filter(d_image, mask, sigma, i):
-	blurred_image = gaussian_filter(d_image, (sigma, sigma, 0))
+	blurred_image = gaussian_filter((d_image / 255.).astype(float), (sigma, sigma, 0))
 
 	blurred_mask = gaussian_filter(mask.astype(float), (sigma, sigma))
 	blurred_mask_3c = np.repeat(np.expand_dims(blurred_mask, 2), 3, axis = 2)
 	blurred_mask_3c_epsilon = np.copy(blurred_mask_3c)
 	blurred_mask_3c_epsilon[np.where(blurred_mask_3c_epsilon == 0)] = 0.00001
-
-	#blurred_mask_stacked = gaussian_filter(mask_stacked.astype(float), (sigma, sigma))
-	#blurred_mask_stacked_3c = np.repeat(np.expand_dims(blurred_mask_stacked, 2), 3, axis = 2)
-	#blurred_mask_stacked_3c_epsilon = np.copy(blurred_mask_stacked_3c)
-	#blurred_mask_stacked_3c_epsilon[np.where(blurred_mask_stacked_3c_epsilon == 0)] = 0.00001
 
 	masked_conv_image = blurred_image / blurred_mask_3c_epsilon
 
@@ -66,7 +65,7 @@ def masked_gaussian_filter(d_image, mask, sigma, i):
 	scipy.misc.toimage(mask, cmin=0., cmax=1.).save(save_path + '{}_6_mask.png'.format(i))
 	scipy.misc.toimage(blurred_image, cmin=0., cmax=255.).save(save_path + '{}_7_image_decomposed_blurred.png'.format(i))
 	scipy.misc.toimage(blurred_mask, cmin=0., cmax=1.).save(save_path + '{}_8_disp_decomposed_binary_blurred.png'.format(i))
-	scipy.misc.toimage(masked_conv_image, cmin=0., cmax=255.).save(save_path + '{}_9_masked_conv_image.png'.format(i))
+	scipy.misc.toimage(masked_conv_image, cmin=0., cmax=1.).save(save_path + '{}_9_masked_conv_image.png'.format(i))
 
 	return masked_conv_image
 
@@ -91,51 +90,49 @@ def get_blurred_images(d_images, d_disp_binary_maps):
 
 	return masked_conv_images
 
-def get_merged_images(m_images, masks):
+def get_b_and_mconv_masks(mask, sigma):
+	blurred_mask = gaussian_filter((mask).astype(float), (sigma, sigma))
+	mconv_mask_epsilon = np.copy(blurred_mask)
+	mconv_mask_epsilon[np.where(mconv_mask_epsilon == 0)] = 0.00001
+	mconv_mask = np.copy(blurred_mask)
+	mconv_mask = mconv_mask / mconv_mask_epsilon
+
+	return blurred_mask, mconv_mask
+
+def get_merged_images(original_image, m_images, masks):
 
 	merged_image = np.zeros_like(m_images[0])
+	mconv_mask_stacked_prev = np.zeros_like(masks[0])
 
 	for i in np.arange(num_layers):
 		if i == 0:
-			merged_image = m_images[i]
-			scipy.misc.toimage(merged_image, cmin=0., cmax=255.).save(save_path + 'test_{}_8_merged_image.png'.format(i))
-			continue;
-		if i + 1 < num_layers:
-			mask_stacked = masks[i] + masks[i + 1]
-		else:
-			mask_stacked = masks[i]
+			merged_image = np.copy(m_images[i])
+			scipy.misc.toimage(merged_image, cmin=0., cmax=1.).save(save_path + 'merge_{}_9_merged_image.png'.format(i))
+			continue
 
-		scipy.misc.toimage(mask_stacked, cmin=0., cmax=1.).save(save_path + 'test_{}_0_mask_stacked.png'.format(i))
+		blurred_mask_stacked, mconv_mask_stacked = get_b_and_mconv_masks(1 - np.sum(np.array(masks)[:i], axis=0), sigma[i])
+		blurred_mask, mconv_mask = get_b_and_mconv_masks(masks[i], sigma[i])
+		scipy.misc.toimage(blurred_mask, cmin=0., cmax=1.).save(save_path + 'merge_{}_1_blurred_mask.png'.format(i))
+		scipy.misc.toimage(mconv_mask, cmin=0., cmax=1.).save(save_path + 'merge_{}_2_mconv_mask.png'.format(i))
+		scipy.misc.toimage(mconv_mask_stacked, cmin=0., cmax=1.).save(save_path + 'merge_{}_2_1_mconv_mask_stacked.png'.format(i))
 
-		blurred_mask_stacked = gaussian_filter(mask_stacked.astype(float), (sigma[i], sigma[i]))
-		scipy.misc.toimage(blurred_mask_stacked, cmin=0., cmax=1.).save(save_path + 'test_{}_0_mask_stacked_blurred.png'.format(i))
+		blurred_mask_prev, mconv_mask_prev = get_b_and_mconv_masks(masks[i - 1], sigma[i - 1])
+		scipy.misc.toimage(mconv_mask_prev, cmin=0., cmax=1.).save(save_path + 'merge_{}_3_mconv_mask_prev.png'.format(i))
 
-		blurred_mask = gaussian_filter(masks[i].astype(float), (sigma[i], sigma[i]))
-		blurred_mask_epsilon = np.copy(blurred_mask)
-		blurred_mask_epsilon[np.where(blurred_mask_epsilon == 0)] = 0.00001
-		blurred_mask = blurred_mask / blurred_mask_epsilon
-		blurred_mask[np.where((blurred_mask * 255).astype(int) == 0)] = 0
+		mconv_mask_stacked_prev = mconv_mask_stacked_prev + mconv_mask_prev - (mconv_mask_stacked_prev * mconv_mask_prev)
+		scipy.misc.toimage(mconv_mask_stacked_prev, cmin=0., cmax=1.).save(save_path + 'merge_{}_4_mconv_mask_stacked_prev.png'.format(i))
 
-		blurred_mask_before = gaussian_filter(masks[i-1].astype(float), (sigma[i-1], sigma[i-1]))
-		blurred_mask_before_epsilon = np.copy(blurred_mask_before)
-		blurred_mask_before_epsilon[np.where(blurred_mask_before_epsilon == 0)] = 0.00001
-		blurred_mask_before = blurred_mask_before / blurred_mask_before_epsilon
-		blurred_mask_before[np.where((blurred_mask_before * 255).astype(int) == 0)] = 0
+		################
+		background = merged_image + original_image * expand_dims((1 - mconv_mask_stacked_prev))
+		foreground = m_images[i] * expand_dims(mconv_mask_stacked) + merged_image * expand_dims(mconv_mask_stacked - mconv_mask)
+		merge_mask = expand_dims(blurred_mask_stacked)
+		merged_image = background * (1 - merge_mask) + foreground * merge_mask
+		################
 
-		scipy.misc.toimage(blurred_mask_before, cmin=0., cmax=1.).save(save_path + 'test_{}_0_mask_blurred_before.png'.format(i))
-		scipy.misc.toimage(blurred_mask, cmin=0., cmax=1.).save(save_path + 'test_{}_0_mask_conv_blurred.png'.format(i))
-
-		blurred_mask = blurred_mask * blurred_mask_before
-		blurred_mask_3c = np.repeat(np.expand_dims(blurred_mask, 2), 3, axis = 2)
-
-		scipy.misc.toimage(blurred_mask, cmin=0., cmax=1.).save(save_path + 'test_{}_1_mask_blurred.png'.format(i))
-		blurred_mask_origin = gaussian_filter(masks[i].astype(float), (sigma[i], sigma[i]))
-		scipy.misc.toimage(masks[i], cmin=0., cmax=1.).save(save_path + 'test_{}_1_mask_origin.png'.format(i))
-		scipy.misc.toimage(blurred_mask_origin, cmin=0., cmax=1.).save(save_path + 'test_{}_1_mask_blurred_origin.png'.format(i))
-
-		merged_image = merged_image * (1 - blurred_mask_3c) + m_images[i] * blurred_mask_3c
-
-		scipy.misc.toimage(merged_image, cmin=0., cmax=255.).save(save_path + 'test_{}_8_merged_image.png'.format(i))
+		scipy.misc.toimage(background, cmin=0., cmax=1.).save(save_path + 'merge_{}_7_background.png'.format(i))
+		scipy.misc.toimage(foreground, cmin=0., cmax=1.).save(save_path + 'merge_{}_9_foreground.png'.format(i))
+		scipy.misc.toimage(merge_mask, cmin=0., cmax=1.).save(save_path + 'merge_{}_10_merge_mask.png'.format(i))
+		scipy.misc.toimage(merged_image, cmin=0., cmax=1.).save(save_path + 'merge_{}_11_merged_image.png'.format(i))
 
 	return merged_image
 
@@ -151,8 +148,8 @@ def main():
 
 	decomposed_images, masks = get_decomposed_images(image, disp_map_norm, num_layers)
 	masked_conv_images = get_blurred_images(decomposed_images, masks)
-	merged_image = get_merged_images(masked_conv_images, masks)
-	scipy.misc.toimage(merged_image, cmin=0., cmax=255.).save(save_path + '00_merged_image.png')
+	merged_image = get_merged_images(image / 255., masked_conv_images, masks)
+	scipy.misc.toimage(merged_image, cmin=0., cmax=1.).save(save_path + '00_merged_image.png')
 
 if __name__ == '__main__':
     main()
